2506bf7cc908a647a0094de8d5a8d964fb82142b
Elnaraa9 <100670042+Elnaraa9@users.noreply.github.com>
2022-03-22 23:50:51 +0400

    Add files via upload

---
+++ Task3.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Hometask3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int number = 45;
+            //Root(ref number);
+
+            string str = "Elnara Vahabova";
+            NotSpace(ref str);
+
+            //int[] nums = { 2,5,4 };
+            //int value = 3;
+            //NewArray(ref nums, value);
+        }
+        //Task3
+        //static void Root(ref int number)
+        //{
+        //    int i = 0;
+        //    while (i*i<=number)
+        //    {
+        //        i++;
+        //    }
+        //    Console.WriteLine(i-1);
+        //}
+
+        //Task2
+        static void NotSpace(ref string str)
+        { 
+            string newname = string.Empty;
+           string name = str.Replace(" ", String.Empty);
+            Console.WriteLine(name);
+        }
+
+        //Task1
+        //static void NewArray(ref int[] array, int value)
+        //{
+        //    int[] newarray = new int[array.Length + 1];
+        //    for (int i = 0; i < array.Length; i++)
+        //    {
+        //        newarray[i] = array[i];
+        //    }
+        //    newarray[array.Length] = value;
+        //    array = newarray;
+        //    foreach (int item in array)
+        //    {
+        //        Console.WriteLine(item);
+        //    }
+        //  }
+
+    }
+
+}
